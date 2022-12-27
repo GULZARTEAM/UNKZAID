645b50044346f4e01695ac603603858228ec1b61
HunterX <108978489+BROTHERHOODOP@users.noreply.github.com>
2022-12-27 19:47:29 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "23748681"))
     API_HASH = os.environ.get("API_HASH", "8ac344c214fbc889c837e9eba866751d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5861787158:AAEhhPB0pK0gN4aIiyIgOfNf7T2DIZZ8V-M")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzoBu7pXzh_zeHiUbKdn1j1ZJ5xqCOFd9BpoFTrkiUUsrPkTeoPK_yHjIyOtjubFEiPP_zp8PjVzKcLmG6Z9A5j8e-TE6XTJfz51Ry52RwbAYbwTcnzw7Qm33f3qh1lnIM3CW-PWx6FTZsDRaSa-CxJ3r-ezwWq4V6nsYpkPG9uxaIU7vuZXky758h0yhcqfP423eBH5NyQ3gR5HyuxKHAYYcPANTkHM7ps3p_WHMpTOEXXT8l7g5oSBaJAsHSIVdUZ4fjrCXb61V8ALXoUy6XzOmUirI1P5gSmddLCjepZAoF8tkQ2mAPrLIbfyy-oP1JTAkRPM0K_3xY04r64-adTO9Ng=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "UNK_MUSIC_BOT")
